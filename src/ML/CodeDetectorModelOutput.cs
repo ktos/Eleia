c50c29b0c85f3040dee07c37a73ecb10f71d8b04
Marcin Badurowicz <m@badurowicz.net>
2019-10-17 00:19:32 +0200

    Change model to a new model created by BinaryClassification AutoML
    New MLModel is based on trainingdata2.tsv, trainingdata2.log is a log
    of mlnet auto-train.

--- src/ML/CodeDetectorModelOutput.cs
+++ src/ML/CodeDetectorModelOutput.cs
@@ -42,8 +42,8 @@ namespace Eleia.ML
         // ColumnName attribute is used to change the column name from
         // its default value, which is the name of the field.
         [ColumnName("PredictedLabel")]
-        public string Prediction { get; set; }
+        public bool Prediction { get; set; }
 
-        public float[] Score { get; set; }
+        public float Score { get; set; }
     }
 }
